d6898964b8567f462b0914a31468d269ae20fadd
LittleLlama246 <114889233+LittleLlama246@users.noreply.github.com>
2022-12-18 18:45:39 -0700

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import './styles/header.css';
+import { useState } from "react";
 import dogHeaderPhoto from "./photos/header.png"
 
-function Header() {
+function Header(props) {
+  console.log("fave", props.fave);
+
+  const [ps, setPS ] = useState(false);
+  const [show, setShow] = useState(false);
+
+
+
+  const handleNavigation = (e) => {
+    const window = e.currentTarget;
+    console.log("y", window.scrollY);
+    if (window.scrollY <= 420) {
+    } else if (window.scrollY >= 300) {
+      setPS(true);
+      this.setState({class: 'fixedElement'})
+    }
+  };
+
+  console.log("fave", show);
+  console.log(ps);
+
+  const componentDidMount = () => {
+    window.scrollTo(0, 0);
+  }
+
+  componentDidMount();
+
   return (
     <div className="Title">
-      <img class="header-img" src={dogHeaderPhoto} width="270" height="230"/>
-      <h1>The Weiner Web</h1>
+      <div class="header-left">
+        <img class="header-img" src={dogHeaderPhoto} width="140" height="100" onScroll={handleNavigation}/>
+        <h1>The Weiner Web</h1>
+      </div>
+      <div class="total"><span id={props.fave ? "show" : "hide"}>Have all your favorite <br></br> Weiner Dogs for: ${props.total}</span></div>
     </div>
   )
 }
